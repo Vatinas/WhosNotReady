4b883c0add3e520777b0df829cfd9b30d55512ee
Vatinas <alexis.laouar@live.fr>
2025-08-03 14:00:02 +0200

    Added zh-cn loc (thanks DingXiang!)

--- scripts/mods/WhosNotReady/WhosNotReady_localization.lua
+++ scripts/mods/WhosNotReady/WhosNotReady_localization.lua
@@ -1,26 +1,34 @@
 return {
 	mod_name = {
 		en = "Who's not ready",
+		["zh-cn"] = "谁没准备好",
 	},
 	mod_description = {
 		en = "During mission votes, displays a notification showing who hasn't voted yet",
+		["zh-cn"] = "在任务投票期间，显示尚未投票的玩家",
 	},
 	name_setting = {
 		en = "Player name displayed",
+		["zh-cn"] = "显示玩家名称",
 	},
 	tooltip_name_setting = {
 		en = "Whether the name displayed for players is their character name, or their account name",
+		["zh-cn"] = "显示角色名还是账户名",
 	},
 	character_name = {
 		en = "Character name",
+		["zh-cn"] = "角色名",
 	},
 	account_name = {
 		en = "Account name",
+		["zh-cn"] = "账户名",
 	},
 	players_not_ready_text = {
 		en = "Players not ready:",
+		["zh-cn"] = "未准备好的玩家：",
 	},
 	name_not_found = {
 		en = "[Player name not found]",
+		["zh-cn"] = "[玩家名称未找到]",
 	},
 }
